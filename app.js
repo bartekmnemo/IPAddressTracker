14d97db90a728f21555dbd8fe9776b9cf8da8e4a
St33v <48390535+bartekmnemo@users.noreply.github.com>
2022-04-26 19:12:08 +0200

    add resposive

--- app.js
+++ app.js
@@ -9,10 +9,36 @@ let lat = 0;
 let lng = 0;
 let userIpAdress = "";
 
+let map;
+
+let searchActive = false;
+
 const API_KEY = "at_OFj4mpAD7hFRCENf4FSGUTCJJ0kDO";
 const ACCESS_TOKEN =
   "pk.eyJ1IjoiYmFydGVrbW5lbW8iLCJhIjoiY2wyZXdwdzNjMDUydzNubmI5NHQyZDR4ayJ9.31JnC-ptw4Ju-_tvpPhC8g";
 
+const getMap = () => {
+  if (!searchActive) {
+    map = L.map("map").setView([lat, lng], 13);
+    L.tileLayer(
+      `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${ACCESS_TOKEN}`,
+      {
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: "mapbox/streets-v11",
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: "your.mapbox.access.token",
+      }
+    ).addTo(map);
+
+    var marker = L.marker([lat, lng]).addTo(map);
+  } else {
+    map.panTo(new L.LatLng(lat, lng));
+    searchActive = false;
+  }
+};
 function getIpInfo(ipAdress) {
   fetch(
     `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${ipAdress}`
@@ -20,27 +46,14 @@ function getIpInfo(ipAdress) {
     .then((res) => res.json())
     .then((data) => {
       ipAdressP.textContent = data.ip;
-      locationP.textContent = `${data.location.city}, ${data.location.country}, ${data.location.postalCode}`;
+      locationP.textContent = `${data.location.city}, ${
+        data.location.country
+      } ${data.location.postalCode ? `,${data.location.postalCode}` : ""}`;
       timezoneP.textContent = `UTC${data.location.timezone}`;
       ispP.textContent = data.isp;
       lat = data.location.lat;
       lng = data.location.lng;
-
-      var map = L.map("map").setView([lat, lng], 13);
-      L.tileLayer(
-        `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${ACCESS_TOKEN}`,
-        {
-          attribution:
-            'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-          maxZoom: 18,
-          id: "mapbox/streets-v11",
-          tileSize: 512,
-          zoomOffset: -1,
-          accessToken: "your.mapbox.access.token",
-        }
-      ).addTo(map);
-
-      var marker = L.marker([51.5, -0.09]).addTo(map);
+      getMap();
     })
     .catch((err) => {
       console.log(JSON.stringify(err));
@@ -62,6 +75,7 @@ getUserIp();
 getIpInfo(userIpAdress);
 
 submitBtn.addEventListener("click", () => {
+  searchActive = true;
   userIpAdress = searchInput.value;
   getIpInfo(userIpAdress);
 });
